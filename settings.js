2e35af4d8b1a20448024b113dd28a8a54247d1f8
SUBZERO-MD <davidpatrf@gmail.com>
2026-03-28 15:11:26 -0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: "Ice~7VHonR", // Put Session Id Here Inside "Ice~3Bsrk2"
+  SESSION_ID: "Ice~pbCcrL", // Put Session Id Here Inside "Ice~3Bsrk2"
   OWNER_NUMBER: "+51907071270", // Put Owner Number Here 
   PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
   CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
